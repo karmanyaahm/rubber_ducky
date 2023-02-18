2553c213608105916e1b97cc4350c88956ca9f23
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2023-02-18 16:51:20 -0600

    better upload

--- circuitpython/upload.sh
+++ circuitpython/upload.sh
@@ -1,8 +1,11 @@
 set -xe
 
+if [ "$1" == "IMG" ]; then
 convert oreo3.png -resize 50% -quality 50 oreo3-1.png
 pngquant oreo3-1.png -f
-python polyglot.py oreo3-1-fs8.png 1oreo.png script.ps1
+fi
+
+python polyglot.py oreo3-1-fs8.png oreo.png script.ps1
 
 set +x
 while ! $(df | grep -q P); do echo waiting; sleep .2; done;
@@ -11,8 +14,14 @@ sleep 2
 set -x
 
 MTPATH="/run/media/karmanyaahm/P"
-cp code.py boot.py 1oreo.png $MTPATH/
-sync $MTPATH $MTPATH/ $MTPATH/code.py $MTPATH/1oreo.png $MTPATH/boot.py
-touch $MTPATH/1oreo.png
+
+# delete all dirs
+find $MTPATH -mindepth 1 -type d -exec rm -rf {} ';' || true
+find $MTPATH -mindepth 1 -type d -exec rm -rf {} ';' || true
+find $MTPATH -mindepth 1 -type d -exec rm -rf {} ';' || true
+
+cp -u code.py boot.py oreo.png $MTPATH/
+sync $MTPATH $MTPATH/ $MTPATH/code.py $MTPATH/oreo.png $MTPATH/boot.py
+touch $MTPATH/oreo.png
 sleep 1 
 umount $MTPATH
