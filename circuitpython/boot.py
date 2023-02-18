1d2ab19f9e533ad4c86eb649f513fbc3a740b756
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2023-02-18 16:48:14 -0600

    further simplify usb interface

--- circuitpython/boot.py
+++ circuitpython/boot.py
@@ -1,4 +1,9 @@
 import storage
 #storage.disable_usb_drive()
 storage.remount("/", False) # writeable by circuitpython, read-only to computer
+import usb_midi
+
+usb_midi.disable()
+import usb_cdc
+usb_cdc.disable()
 
